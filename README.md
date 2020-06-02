2b80b399daceca1ab1c973dc435e181fd9b7e35a
0E95E69BD3976E8B934E8AD3B572B3DE <0E95E69BD3976E8B934E8AD3B572B3DE@i-search.com.cn>
2020-06-02 11:05:29 +0800

    guojie
    添加代码

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# 计算定时任务时间
- 计算定时任务时间
+#    auto_time 定时时间
+#    auto_tag 是否开启定时
+#    auto_cycle 1 每天 2 每周 3 每月
+#    auto_date 定时任务日期
---
+++ main.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+"""
+@author:'guojie'
+@Email:jie.guo@msxf.com
+@createtime:2020/06/02
+"""
+import datetime
+import time
+
+
+def computing_time(auto_conf):
+    now_time = time.strftime("%H:%M:%S", time.localtime())
+    left_days = 0
+    today = datetime.date.today()
+    # print(today)
+    # today = "2021-07-31"
+    # today = datetime.datetime.strptime(today, "%Y-%m-%d")
+    if auto_conf['auto_tag'] == 1:
+        if auto_conf['auto_cycle'] == 1:
+            left_days = 1
+        elif auto_conf['auto_cycle'] == 2:
+            auto_rule = auto_conf['auto_date']
+            now_mday = time.strftime("%w", time.localtime())
+            if now_mday == 0:
+                now_mday = 7
+            for temp_mday in auto_rule:
+                if int(now_mday) <= temp_mday:
+                    if int(now_mday) == 7:
+                        left_days = int(auto_rule[0])
+                    elif int(now_mday) == int(temp_mday):
+                        left_days = int(
+                            auto_rule[(auto_rule.index(temp_mday) + 1) % len(auto_rule)] - int(now_mday) + (
+                                    auto_rule.index(temp_mday) + 1) / len(auto_rule) * 7)
+                    elif int(temp_mday) > int(now_mday):
+                        left_days = int(temp_mday) - int(now_mday)
+                    break
+            if not left_days:
+                left_days = int(auto_rule[0]) + 7 - int(now_mday)
+        elif auto_conf['auto_cycle'] == 3:
+            auto_rule = auto_conf['auto_date']
+            now_day = time.strftime("%d", time.localtime())
+            now_month = time.strftime("%m", time.localtime())
+            key_29 = [2]
+            key_30 = [4, 6, 9, 11]
+            if int(now_month) in key_29:
+                key_num = 28
+            elif int(now_month) in key_30:
+                key_num = 30
+            else:
+                key_num = 31
+            for temp_day in auto_rule:
+                if int(now_day) <= temp_day:
+                    if int(now_day) == temp_day:
+                        left_days = int(
+                            auto_rule[(auto_rule.index(temp_day) + 1) % len(auto_rule)] - int(now_day) +
+                            int((auto_rule.index(temp_day) + 1) / len(auto_rule)) * key_num)
+                    elif int(now_day) < temp_day:
+                        left_days = int(temp_day - int(now_day))
+                    break
+            if not left_days:
+                left_days = int(auto_rule[0]) + key_num - int(now_day)
+        if now_time > auto_conf['auto_time']:
+            nextTime = str(
+                today + datetime.timedelta(days=left_days)) + ' ' + auto_conf['auto_time']
+        else:
+            nextTime = str(datetime.date.today()) + ' ' + auto_conf['auto_time']
+
+        return nextTime
+
+
+if __name__ == '__main__':
+    computing_time({"auto_time": "10:40:00", "auto_tag": 1, "auto_cycle": 1, "auto_date": [31]})
